bdb2f87b9b1628657229328ad1f797c471493ed5
kkh5428@gmail.com <KaniKim>
2022-01-30 10:59:18 +0900

    add models schema to rails mysql

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_102017) do
+ActiveRecord::Schema.define(version: 2022_01_30_015830) do
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+  create_table "bonds", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.string "state"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "friend_id"], name: "index_bonds_on_user_id_and_friend_id", unique: true
+  end
+
+  create_table "pictures", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.string "caption"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "fk_rails_13793e98fd"
+  end
+
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "thread_id"
+    t.string "postable_type", null: false
+    t.bigint "postable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["postable_type", "postable_id"], name: "index_posts_on_postable"
+    t.index ["thread_id"], name: "fk_rails_79cf8a9f2a"
+    t.index ["user_id"], name: "fk_rails_5b5ddfd518"
+  end
+
+  create_table "statuses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "usernmae"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.boolean "is_public"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["usernmae"], name: "index_users_on_usernmae", unique: true
+  end
+
+  add_foreign_key "pictures", "posts"
+  add_foreign_key "posts", "posts", column: "thread_id"
+  add_foreign_key "posts", "users"
 end
