4d88e68bf2bccf67689b4e40311eb1d05c201057
KaniKim <kkh5428@gmail.com>
2022-02-26 18:20:58 +0900

    git clean cache

---
+++ hello/app/controllers/hello_controller.rb
@@ -0,0 +1,16 @@
+class HelloController < ApplicationController
+    
+    def index
+        if request.post? then
+            @title = "Result"
+            if params["r1"] then
+                @msg = "you selected: " + params["r1"]
+            else
+                @msg = "not selected..."
+            end
+        else
+            @title = "Index"
+            @msg = "select radio button..."
+        end
+    end
+end
---
+++ hello/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'hello/index'
+  get 'hello/other'
+  post 'hello', to: 'hello#index'
+  post 'hello/index'
+  get 'hello', to: 'hello#index'
+end
