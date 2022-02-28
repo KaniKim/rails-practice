95bc72e0475a11b0e05adac8d6cf2ccb3b583635
KaniKim <kkh5428@gmail.com>
2022-02-28 21:59:27 +0900

    add new controller and erb

--- hello/config/routes.rb
+++ hello/config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
 
   get 'people/index'
   get 'people', to: 'people#index'
+  get 'people/add'
+  post 'people/add', to: 'people#create'
   get 'people/:id', to: 'people#show'
 end
